c06fd4aa54031e8cca6a38b6a536cfbfea746ef8
Raphael <raphael@skyleafdesign.com>
2015-05-29 14:52:35 -0500

    Added 'services' HTML, background-cover fix, new homepage region.

--- plugins/panels/layouts/spartan_homepage/spartan-homepage.tpl.php
+++ plugins/panels/layouts/spartan_homepage/spartan-homepage.tpl.php
@@ -2,7 +2,8 @@
 <div class="<?php print $classes ?>" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
 
   <?php if (!empty($content['slogan']) || !empty($content['hero'])): ?>
-    <div class="hero-container">
+    <div id="hero-background" class="css3-background-fix"></div><div class="hero-container">
+
       <div class="container">
 
         <?php if (!empty($content['slogan'])): ?>
@@ -28,13 +29,21 @@
   <?php if ($content['upper_left'] || $content['upper_middle'] || $content['upper_right']): ?>
     <div class="section-container">
       <div class="row">
-        <div class="col-sm-4">
+        <div class="col-sm-8 col-sm-offset-2">
+          <h2 id="explanation"><em>What can a knowledge platform do for <strong>you</strong>?&nbsp;</em></h2>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm-6 col-md-3">
           <?php print $content['upper_left']; ?>
         </div>
-        <div class="col-sm-4">
-          <?php print $content['upper_middle']; ?>
+        <div class="col-sm-6 col-md-3">
+          <?php print $content['upper_middle_left']; ?>
         </div>
-        <div class="col-sm-4">
+        <div class="col-sm-6 col-md-3">
+          <?php print $content['upper_middle_right']; ?>
+        </div>
+        <div class="col-sm-6 col-md-3">
           <?php print $content['upper_right']; ?>
         </div>
       </div>
